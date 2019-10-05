6437c4a118a139bbab005d632b92baa606cfb82f
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-10-05 11:22:11 -0300

    02-10 Construindo a interface web.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -8,7 +11,9 @@ mongoose.connect('mongodb+srv://omnistack:semanaOmnistack@freecluster0-5bnfk.gcp
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
